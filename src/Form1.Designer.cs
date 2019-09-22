1005eb200c834218372efc18c175a2c867fe100f
Jared Barboza <jared.m.barboza@gmail.com>
2019-09-22 16:33:34 -0400

    initial commit

---
+++ src/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace LapseCapture {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.CaptureIntervalChooser = new System.Windows.Forms.ComboBox();
+            this.StartCapture = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            //
+            // label1
+            //
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Capture Interval";
+            //
+            // CaptureIntervalChooser
+            //
+            this.CaptureIntervalChooser.FormattingEnabled = true;
+            this.CaptureIntervalChooser.Items.AddRange(new object[] {
+            "30 Seconds",
+            "1 Minute",
+            "5 Minutes"});
+            this.CaptureIntervalChooser.Location = new System.Drawing.Point(100, 6);
+            this.CaptureIntervalChooser.Name = "CaptureIntervalChooser";
+            this.CaptureIntervalChooser.Size = new System.Drawing.Size(121, 21);
+            this.CaptureIntervalChooser.TabIndex = 1;
+            //
+            // StartCapture
+            //
+            this.StartCapture.Location = new System.Drawing.Point(227, 4);
+            this.StartCapture.Name = "StartCapture";
+            this.StartCapture.Size = new System.Drawing.Size(92, 23);
+            this.StartCapture.TabIndex = 2;
+            this.StartCapture.Text = "Start Capture";
+            this.StartCapture.UseVisualStyleBackColor = true;
+            //
+            // Form1
+            //
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 37);
+            this.Controls.Add(this.StartCapture);
+            this.Controls.Add(this.CaptureIntervalChooser);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Screen Lapse";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox CaptureIntervalChooser;
+        private System.Windows.Forms.Button StartCapture;
+    }
+}
+
---
+++ src/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace LapseCapture {
+    public partial class Form1 : Form {
+
+        private static readonly int[] _intervals = new int[] {
+            30000,
+            60000,
+            300000
+        };
+
+        private readonly BackgroundWorker _worker;
+
+        public Form1() {
+            InitializeComponent();
+            _worker = new BackgroundWorker();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            CaptureIntervalChooser.SelectedIndex = 0;
+            StartCapture.Click += StartCapture_Click;
+        }
+
+        private void StartCapture_Click(object sender, EventArgs e) {
+            if (_worker.IsBusy) {
+                StartCapture.Text = "Start Capture";
+                _worker.CancelAsync();
+            } else {
+                _worker.DoWork += Worker_DoWork;
+                _worker.WorkerSupportsCancellation = true;
+                _worker.RunWorkerAsync();
+                WindowState = FormWindowState.Minimized;
+                StartCapture.Text = "Cancel";
+            }
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e) {
+            while (true) {
+                if (_worker.CancellationPending) {
+                    e.Cancel = true;
+                    return;
+                }
+
+                int interval = GetIntervalMilliseconds();
+                Thread.Sleep(interval);
+
+                Bitmap bmpScreenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
+                               Screen.PrimaryScreen.Bounds.Height,
+                               PixelFormat.Format32bppArgb);
+                Graphics gfxScreenshot = Graphics.FromImage(bmpScreenshot);
+                gfxScreenshot.CopyFromScreen(Screen.PrimaryScreen.Bounds.X,
+                            Screen.PrimaryScreen.Bounds.Y,
+                            0,
+                            0,
+                            Screen.PrimaryScreen.Bounds.Size,
+                            CopyPixelOperation.SourceCopy);
+
+                bmpScreenshot.Save($"{DateTimeOffset.Now.ToUnixTimeSeconds()}.png", ImageFormat.Png);
+            }
+        }
+
+        private int GetIntervalMilliseconds() {
+            return _intervals[CaptureIntervalChooser.SelectedIndex];
+        }
+    }
+}
